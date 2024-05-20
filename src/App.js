85844d6e454de3e7e9d7b633d732995564612725
SaraAbdo96 <sara.nadim.abdo@gmail.com>
2024-05-21 02:23:19 +0300

    todo-react-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Task from "./Task";
+import TaskForm from "./TaskForm";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    if (tasks.length === 0) return;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    setTasks(tasks);
+  }, []);
+
+  function addTask(name) {
+    setTasks((prev) => {
+      return [...prev, { name: name, done: false }];
+    });
+  }
+
+  function removeTask(indexToRemove) {
+    setTasks((prev) => {
+      return prev.filter((taskObject, index) => index !== indexToRemove);
+    });
+  }
+
+  function updateTaskDone(taskIndex, newDone) {
+    setTasks((prev) => {
+      const newTasks = [...prev];
+      newTasks[taskIndex].done = newDone;
+      return newTasks;
+    });
+  }
+
+  const numberComplete = tasks.filter((task) => task.done).length;
+  const numberTotal = tasks.length;
+
+  function getMessage() {
+    const percentage = (numberComplete / numberTotal) * 100;
+    if (percentage === 0) {
+      return "try to do at least one!";
+    }
+    if (percentage === 100) {
+      return "Nice job for today";
+    }
+    return "Keep it going ";
+  }
+
+  function renameTask(index, newName) {
+    setTasks((prev) => {
+      const newTasks = [...prev];
+      newTasks[index].name = newName;
+      return newTasks;
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>
+        {numberComplete}/{numberTotal} Complete
+      </h1>
+      <h2>{getMessage()}</h2>
+      <TaskForm onAdd={addTask} />
+      {tasks.map((task, index) => (
+        <Task
+          {...task}
+          onToggle={(done) => updateTaskDone(index, done)}
+          onTrash={() => removeTask(index)}
+          onRename={(newName) => renameTask(index, newName)}
+        />
+      ))}
+    </main>
   );
 }
 
